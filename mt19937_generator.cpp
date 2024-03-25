79e33bb52e23b6427a87ba3ee3398cce58b07bba
Daniil <87397419+fitopisya@users.noreply.github.com>
2024-03-25 21:33:38 +0300

    Create mt19937_generator.cpp

---
+++ mt19937_generator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <random> 
+#include <algorithm>
+using namespace std;
+
+int main() {
+    static const int NUMBERS = 1'000;
+    mt19937 generator;
+    uniform_int_distribution<int> uniform_dist(0, 50);
+    vector<int> nums;
+    for (int i = 0; i < NUMBERS; ++i) {
+        int random_number = uniform_dist(generator);
+        nums.push_back(random_number);
+    }
+    sort(nums.begin(), nums.end());
+    for (const auto el : nums){
+       cout << el << ' ';
+    }
+}
